26f1a4956401ca0ed7e290ee9fbfd855a1ba8ddb
wqq <417513676@qq.com>
2018-09-02 23:36:04 +0800

    ==

--- OCProject/OCProject/TestConstVC/TestConstVC.m
+++ OCProject/OCProject/TestConstVC/TestConstVC.m
@@ -31,7 +31,6 @@
     p = &c;
     
     
-    
     //下面的两种写法一样
     const int *d = p;
     NSLog(@"d===%p",d);
@@ -44,10 +43,11 @@
     e = &b;
     
     
-    
     int * const f = &a;
     NSLog(@"f===%p",f);
     //f = &a;//报错，不能修改f的值
+    
+    
     //下面的两种写法一样
     const int *const k = &c;
     NSLog(@"k===%p",k);
@@ -57,14 +57,15 @@
 //    *k = 80;//报错
 //    j = &b;//报错
 //    *j = 80;//报错
-    //上述的两种写法是一样的，第一个const修改的是*k和*j，所以*k和*j不能修改，第二个const修饰的是变量k和j（k和j）都是指针变量，他们的值也不能修改
+    //上述的两种写法是一样的，第一个const修改的是*k和*j，所以*k和*j不能修改，第二个const修饰的是变量k和j（k和j都是指针变量，他们的值也不能修改）
+    
     [self test:&a];
     
     [self test2:&a];
 }
 - (void)test:(const int *)ak{
     NSLog(@"ak===%p",ak);
-    //*ak = 100;//会报错，const修饰的是（*ak），即 指针变量指向额内存空间存贮的值
+    //*ak = 100;//会报错，const修饰的是（*ak），即 指针变量指向的内存空间存贮的值不能修改
     int o = 100;
     ak = &o;//但是可以修改指针的值
     NSLog(@"ak===%p",ak);
@@ -73,7 +74,6 @@
     int pp = 100;
     //bk = &pp;//报错，const修饰的是bk指针
     *bk = pp;//但是可以修改bk指针指向的空间存储的值
-    
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
